1c079d3640bed6cb11f5f5b713d601b61c021be2
Robert Eriksson <robert.eriksson1@comhem.com>
2019-09-11 13:41:37 +0200

    remove unused

--- src/main/java/application/MainController.java
+++ src/main/java/application/MainController.java
@@ -21,18 +21,6 @@ public class MainController {
         return "Uppe och snurrar!\n";
     }
 
-    @RequestMapping("/newproducts/1")
-    public String newProducts1() throws JsonProcessingException {
-        ProductRelease release = restTemplate.getForObject("https://systembevakningsagenten.se/api/json/1.0/newProducts.json", ProductRelease.class);
-        return new ObjectMapper().writeValueAsString(release);
-    }
-
-    @RequestMapping("newproducts/2")
-    public String newProducts2() throws JsonProcessingException {
-        ProductReleaseList productReleaseList = restTemplate.getForObject("https://systembevakningsagenten.se/api/json/2.0/newProducts.json", ProductReleaseList.class);
-        return new ObjectMapper().writeValueAsString(productReleaseList);
-    }
-
     @RequestMapping("/uppgift/1")
     public Object uppgift1() {
         StoreInventory storeInventory = restTemplate.getForObject("https://systembevakningsagenten.se/api/json/1.0/inventoryForStore.json?id=1337", StoreInventory.class);
